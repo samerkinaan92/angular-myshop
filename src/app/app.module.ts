28f77eb5c82b9764d1e996ae3217167d6afd08d4
samer kinaan <samerkinaan@gmail.com>
2019-08-09 00:42:48 +0300

    fisrt commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { CategoriesListComponent } from './categories-list/categories-list.component';
+import { CategoryItemComponent } from './category-item/category-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ProductsPageComponent,
+    CategoriesListComponent,
+    CategoryItemComponent
   ],
   imports: [
     BrowserModule,
