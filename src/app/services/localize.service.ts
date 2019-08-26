8385e3976b9a8f9e872cdbbb6e4d80e18c698802
samer kinaan <samerkinaan@gmail.com>
2019-08-26 14:13:39 +0300

    added service for localization

---
+++ src/app/services/localize.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { languages } from '../../assets/lang/lang.json'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalizeService {
+
+  private langs: string[] = ['en', 'he'];
+  private words: object = null;
+
+  constructor() { }
+
+  //returns a list of available languages
+  getLangs(): string[]{
+    return this.langs;
+  }
+
+  setLang(lang: string): void{
+    if(languages.hasOwnProperty(lang)){
+      this.words = languages[lang];
+    }else{
+      //if property is not found it defaults to english
+      this.words = null;
+    }
+  }
+
+  getLocalizedStr(key: string): string{
+    if(this.words == null){
+      return key;
+    }else if(this.words.hasOwnProperty(key)){
+      return this.words[key];
+    }else{
+      return key;
+    }
+  }
+}
